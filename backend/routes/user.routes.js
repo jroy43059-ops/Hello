d0ac2616b0946740d493e748c0f0d2254f88392c
jroy43059-ops <jroy43059@gmail.com>
2025-12-29 11:28:21 +0530

    Final fix: Switched to bcryptjs and updated dependencies

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const { userModel } = require("../model/user.model")
 const jwt = require("jsonwebtoken")
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userRoute = express.Router()
 
